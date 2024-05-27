73954f45b10f297dc27077417b8ca00a8dd361e6
yan-roger-fv <yanrogerfv@gmail.com>
2024-05-27 11:25:31 -0300

    init

---
+++ src/main/java/themed/park/ThemedParkApplication.java
@@ -0,0 +1,13 @@
+package themed.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemedParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemedParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/themed/park/ThemedParkApplicationTests.java
@@ -0,0 +1,13 @@
+package themed.park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemedParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
